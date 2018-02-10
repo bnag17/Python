6f9ab65ce9156df94aeab9ab8a546d61c77860c8
nageshnaidu555 <35717989+nageshnaidu555@users.noreply.github.com>
2018-02-10 09:27:59 +0530

    Add files via upload

---
+++ mylist nag.py
@@ -0,0 +1,12 @@
+mylist = [0,1, 2, 3, 4]
+mylist[0] = 'early morning'
+mylist[1] = 'yoga'
+mylist[2] = 'bath'
+mylist[3] = 'breakfast'
+
+a=('my daily activities, ill wake up')
+b=('then,I will do ')
+c=('and ill complete')
+d=('ill get ready for')
+e=('ill use')
+print(a,mylist[0],b,mylist[1],c,mylist[2],d,mylist[3],)
